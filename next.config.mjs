28946797418ca30641b0dd5ceaa232392db45116
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-04 16:12:22 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   async headers() {
-    return [
-      {
-        source: '*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ];
-  },
+   
 };
 
 export default nextConfig;
