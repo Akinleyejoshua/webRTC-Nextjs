2951cd65c161e41407eb45741786e27eb0c295d7
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-04 15:43:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   async headers() {
+    return [
+      {
+        source: 'https://ultrashare-api.vercel.app',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://ultrashare-api.vercel.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
