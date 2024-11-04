3f4f13771b7d9b0bd609e5e12843295682ed58ea
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-04 16:14:57 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   async headers() {
+    return [
+      {
+        source: 'https://ultrashare-api.onrender.com',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://ultrashare-api.onrender.com' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
    
 };
 
